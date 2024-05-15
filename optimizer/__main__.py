6892f3db4fbb0e8587931dd0b82a77c549570864
Itsuki Sakitsu <itsuki.sakitsu@gmail.com>
2024-05-16 00:37:03 +0900

    有給希望を複雑化

--- optimizer/__main__.py
+++ optimizer/__main__.py
@@ -17,8 +17,25 @@ if __name__ == '__main__':
     from .cli import main
     # 有給取得希望日を設定
     pto_requests = [
-        PaidTimeOffRequest(Role.ER, [datetime.date(2024, 4, 5), datetime.date(2024, 4, 6)]),
-        PaidTimeOffRequest(Role.ICU, [datetime.date(2024, 4, 1), datetime.date(2024, 4, 2)]),
-        PaidTimeOffRequest(Role.ICU, [datetime.date(2024, 4, 15), datetime.date(2024, 4, 16)]),
+        PaidTimeOffRequest(Role.ICU, [datetime.date(2024, 6, 8), datetime.date(2024, 6, 9), datetime.date(2024, 6, 7), datetime.date(2024, 6, 21), datetime.date(2024, 6, 22), datetime.date(2024, 6, 28)]),
+        PaidTimeOffRequest(Role.ICU, [datetime.date(2024, 6, 7), datetime.date(2024, 6, 14), datetime.date(2024, 6, 8), datetime.date(2024, 6, 9), datetime.date(2024, 6, 15), datetime.date(2024, 6, 16), datetime.date(2024, 6, 30)]),
+        PaidTimeOffRequest(Role.ER, [datetime.date(2024, 6, 1), datetime.date(2024, 6, 8), datetime.date(2024, 6, 15), datetime.date(2024, 6, 22), datetime.date(2024, 6, 23), datetime.date(2024, 6, 29), datetime.date(2024, 6, 9)]),
+        PaidTimeOffRequest(Role.ER, [datetime.date(2024, 6, 23), datetime.date(2024, 6, 8), datetime.date(2024, 6, 15), datetime.date(2024, 6, 22), datetime.date(2024, 6, 1), datetime.date(2024, 6, 5), datetime.date(2024, 6, 12)]),
+        PaidTimeOffRequest(Role.ER, [datetime.date(2024, 6, 27), datetime.date(2024, 6, 28), datetime.date(2024, 6, 29), datetime.date(2024, 6, 30)]),
+        PaidTimeOffRequest(Role.ICU, [datetime.date(2024, 6, 8), datetime.date(2024, 6, 15), datetime.date(2024, 6, 22), datetime.date(2024, 6, 23)]),
+        PaidTimeOffRequest(Role.ER, [datetime.date(2024, 6, 2), datetime.date(2024, 6, 26), datetime.date(2024, 6, 27), datetime.date(2024, 6, 28), datetime.date(2024, 6, 29), datetime.date(2024, 6, 30)]),
+        PaidTimeOffRequest(Role.ICU, [datetime.date(2024, 6, 8), datetime.date(2024, 6, 22), datetime.date(2024, 6, 23)]),
+        PaidTimeOffRequest(Role.ER, [datetime.date(2024, 6, 2), datetime.date(2024, 6, 13), datetime.date(2024, 6, 20)]),
+        PaidTimeOffRequest(Role.ER, [datetime.date(2024, 6, 1), datetime.date(2024, 6, 15), datetime.date(2024, 6, 16), datetime.date(2024, 6, 17), datetime.date(2024, 6, 18), datetime.date(2024, 6, 19), datetime.date(2024, 6, 2)]),
+        PaidTimeOffRequest(Role.ER, [datetime.date(2024, 6, 24), datetime.date(2024, 6, 30)]),
+        PaidTimeOffRequest(Role.ER, [datetime.date(2024, 6, 2), datetime.date(2024, 6, 9), datetime.date(2024, 6, 15), datetime.date(2024, 6, 16), datetime.date(2024, 6, 22), datetime.date(2024, 6, 23), datetime.date(2024, 6, 29)]),
+        PaidTimeOffRequest(Role.ER, [datetime.date(2024, 6, 1), datetime.date(2024, 6, 2), datetime.date(2024, 6, 3), datetime.date(2024, 6, 8), datetime.date(2024, 6, 9), datetime.date(2024, 6, 16), datetime.date(2024, 6, 21)]),
+        PaidTimeOffRequest(Role.ER, [datetime.date(2024, 6, 3), datetime.date(2024, 6, 7), datetime.date(2024, 6, 9), datetime.date(2024, 6, 15), datetime.date(2024, 6, 22), datetime.date(2024, 6, 29)]),
+        PaidTimeOffRequest(Role.ER, [datetime.date(2024, 6, 1), datetime.date(2024, 6, 7), datetime.date(2024, 6, 12), datetime.date(2024, 6, 22), datetime.date(2024, 6, 23), datetime.date(2024, 6, 29), datetime.date(2024, 6, 30)]),
+        PaidTimeOffRequest(Role.ICU, [datetime.date(2024, 6, 3), datetime.date(2024, 6, 28), datetime.date(2024, 6, 29), datetime.date(2024, 6, 30)]),
+        PaidTimeOffRequest(Role.ICU, [datetime.date(2024, 6, 6), datetime.date(2024, 6, 23)]),
+        PaidTimeOffRequest(Role.ER, [datetime.date(2024, 6, 1), datetime.date(2024, 6, 14), datetime.date(2024, 6, 15)]),
+        PaidTimeOffRequest(Role.ER, [datetime.date(2024, 6, 1), datetime.date(2024, 6, 6), datetime.date(2024, 6, 7), datetime.date(2024, 6, 8)]),
+        PaidTimeOffRequest(Role.ER, [datetime.date(2024, 6, 11), datetime.date(2024, 6, 27), datetime.date(2024, 6, 28), datetime.date(2024, 6, 29), datetime.date(2024, 6, 30)]),
     ]
     main(args.year, args.month, args.er_count, args.icu_count, args.max_attempt, pto_requests)
